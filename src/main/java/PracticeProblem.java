2e3b6935099427460b36364b2e9a199836f3e3ac
JackyLui1215 <343344461@tdsb.ca>
2026-03-11 15:46:31 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,42 @@
+/**
+ * File: More Functions 
+ * Author: Jacky Lui
+ * Date Last Modified: Mar 11, 2026
+ */
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+	
 	}
+
+	public static boolean isEven(int number) {
+		boolean bool = ((number % 2) == 0);
+		return bool;
+		
+	}
+
+	public static boolean isOdd(int number) {
+		boolean bool = ((number % 2) != 0);
+		return bool;
+	}
+
+	public static boolean isPositive(int number) {
+		boolean positive = ((number > 0));
+		return positive;
+	}
+
+	public static boolean isNegative(int number) {
+		boolean negative = (number < 0);
+		return negative;
+	}
+
+	public static int combinedLength(String word1, String word2) {
+		int length = ((word1.length()) + word2.length());
+		return length;
+	}
+
+
 
 }
